5cac8a25c35190512c837da309c73bdbba467ac9
rajanmagratitsk <magratirjn9815@gmail.com>
2026-02-27 12:00:50 +0545

    wordpattern

---
+++ WordPattern.java
@@ -0,0 +1,16 @@
+
+
+
+public class WordPattern {
+    public static void main(String[]args){
+
+    String word="COMPUTERMECHANICS";
+    for(int i=1;i<=word.length();i++){
+        for(int j=0;j<i;j++){
+            System.out.print(word.charAt(j));
+        }
+        System.out.println("");
+    
+}
+}
+}
