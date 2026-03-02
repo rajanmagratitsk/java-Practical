772a96ea26b2339ddb046fe30ab87d3448250a87
rajanmagratitsk <magratirjn9815@gmail.com>
2026-03-02 08:37:56 +0545

    demo

---
+++ SynchronizationDemo.java
@@ -0,0 +1,47 @@
+class Table {
+
+    // synchronized method
+    synchronized void printTable(int n) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+// Thread class
+class MyThread1 extends Thread {
+    Table t;
+    MyThread1(Table t) {
+        this.t = t;
+    }
+    public void run() {
+        t.printTable(5);
+    }
+}
+
+class MyThread2 extends Thread {
+    Table t;
+    MyThread2(Table t) {
+        this.t = t;
+    }
+    public void run() {
+        t.printTable(10);
+    }
+}
+
+public class SynchronizationDemo {
+    public static void main(String[] args) {
+        Table obj = new Table();   // shared object
+
+        MyThread1 t1 = new MyThread1(obj);
+        MyThread2 t2 = new MyThread2(obj);
+
+        t1.start();
+        t2.start();
+    }
+}
