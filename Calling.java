7ceae96d535738c30963f9c3cde492db9aae6b93
rajanmagratitsk <magratirjn9815@gmail.com>
2026-03-07 08:34:26 +0545

    message using method

---
+++ Calling.java
@@ -0,0 +1,14 @@
+class Calling {
+
+    void message() {
+        System.out.println("Hello Rajan");
+        System.out.println("Good morning");
+    }
+
+    public static void main(String[] args) {
+
+        Calling obj = new Calling();   // create object
+        obj.message();           // call method
+
+    }
+}
