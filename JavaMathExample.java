2edc54a91e2c889d169b2b161123cdecb855b405
rajanmagratitsk <magratirjn9815@gmail.com>
2026-03-10 22:26:28 +0545

    math

---
+++ JavaMathExample.java
@@ -0,0 +1,28 @@
+
+public class JavaMathExample {
+    public static void main(String args[]){
+    double x=28;
+    double y=4;
+    System.out.println("jmaximum number of x and y is :"+Math.max(x,y));
+    // return square root of y
+    System.out.println("Square root y is:"+Math.sqrt(y));
+    //return 28 power of 4 i.e 
+    System.out.println("Power of x and y is:"+Math.pow(x,y));
+    //return the logarithm of given value
+    System.out.println("logarithm of x is:"+Math.log(x));
+    System.out.println("logarithm of y is:"+Math.log(y));
+    //return of logarithm of given value when baseis 10
+    System.out.println("log 10 of x is :"+Math.log10(x));
+    System.out.println("log 10 of y is :"+Math.log10(y));
+    // return the log of x+1
+    System.out.println("log 1p of x is:"+Math.log1p(x));
+    //return a power of 2
+      System.out.println("exp of x is:"+Math.exp(x));
+      //return(x power of 2)-1
+      System.out.println("expm1 of x is :"+Math.expm1(x));
+      
+    
+            
+    
+    }
+}
