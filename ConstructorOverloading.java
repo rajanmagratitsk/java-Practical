99fecc81474f83a13783b5aed9eb91212006d88d
rajanmagratitsk <magratirjn9815@gmail.com>
2026-04-16 16:57:18 +0545

    constructor overloading

---
+++ ConstructorOverloading.java
@@ -0,0 +1,30 @@
+public class ConstructorOverloading {
+int a;
+String str;
+int b;
+     ConstructorOverloading() {
+        System.out.println("hello");
+    }
+    ConstructorOverloading(int a, String str){
+        this.a=a;
+        this.str=str;
+    }
+
+     ConstructorOverloading(int a, String str, int b) {
+        this.a = a;
+        this.str = str;
+        this.b = b;
+    }
+    void display(){
+        System.out.println("int is:"+a+"String is:"+str+"int is:"+b);
+
+    }
+    public static void main(String[] args) {
+        ConstructorOverloading cog=new ConstructorOverloading();
+        ConstructorOverloading cog1=new ConstructorOverloading(1, "rajan");
+        ConstructorOverloading cog2=new ConstructorOverloading(1, "nichyo", 4);
+        cog.display();
+        cog1.display();
+        cog2.display();
+    }
+}
