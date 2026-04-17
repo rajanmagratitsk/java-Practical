85cc59341da930f141ea91347603f478b4c3743f
rajanmagratitsk <magratirjn9815@gmail.com>
2026-04-17 09:46:48 +0545

    exception handling

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+            System.out.println(a);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
