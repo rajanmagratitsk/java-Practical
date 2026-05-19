68e852c6074269bad3253beaa12d8adf04907e2b
rajanmagratitsk <magratirjn9815@gmail.com>
2026-05-19 19:49:24 +0545

    length

---
+++ StringLengh.java
@@ -0,0 +1,9 @@
+class StringLength {
+    public static void main(String[] args) {
+
+        String name = "Rajan";
+
+        System.out.println("String = " + name);
+        System.out.println("Length = " + name.length());
+    }
+}
